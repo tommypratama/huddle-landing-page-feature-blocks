90d17c8cae16808bec144d607d44f3006ab51abd
Tommy Pratama <tommyhpratama@gmail.com>
2020-09-26 17:37:39 +0700

    Updated baseurl gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/huddle-landing-page-feature-blocks/'
-    : '/'
-}
+  base: "/huddle-landing-page-feature-blocks/",
+};
