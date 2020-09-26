e9c0ddbc27481b70c3efdca39e9db760164a345d
Tommy Pratama <tommyhpratama@gmail.com>
2020-09-26 12:08:09 +0700

    :octocat: Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+## Install Project
+
+```bash
+yarn create vite-app <project-name>
+cd <project-name>
+yarn
+yarn dev
+```
+
+**Add Tailwindcss**
+
+```bash
+yarn add -D tailwindcss autoprefixer postcss-cli
+```
+
---
+++ tailwind.config.js
@@ -0,0 +1,58 @@
+/*
+ ** TailwindCSS Configuration File
+ **
+ ** Docs: https://tailwindcss.com/docs/configuration
+ ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+ */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+	future: {
+		removeDeprecatedGapUtilities: true,
+		purgeLayersByDefault: true,
+	},
+	purge: {
+		enabled: process.env.NODE_ENV === 'production',
+		content: ['src/**/*.vue', 'src/**/*.js', 'index.html'],
+	},
+	theme: {
+		fontFamily: {
+			display: ['Poppins', 'sans-serif'],
+			body: ['Open Sans', 'sans-serif'],
+		},
+		fontSize: {
+			xs: '0.75rem',
+			sm: '0.875rem',
+			base: '1rem',
+			lg: '1.125rem',
+			xl: '1.25rem',
+			'2xl': '1.5rem',
+			'3xl': '1.75rem',
+			'4xl': '2.25rem',
+			'5xl': '3rem',
+			'6xl': '4rem',
+		},
+		extend: {
+			spacing: {
+				7: '1.75rem',
+				9: '2.25rem',
+				11: '2.75rem',
+				13: '3.25rem',
+				14: '3.5rem',
+				15: '3.75rem',
+				72: '18rem',
+				84: '21rem',
+				96: '24rem',
+			},
+			colors: {
+				'primary-pink': '#FF52BF',
+				'very-pale-cyan': '#EBFBFF',
+				'very-dark-cyan': '#00252E',
+				'grayish-blue': '#808D99',
+			},
+		},
+	},
+	variants: {},
+	plugins: [],
+};
